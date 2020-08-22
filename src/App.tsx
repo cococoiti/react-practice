b6ec44b8f48945d21b5b52d7822b7b6dac103ca0
cococoiti <cococoiti@icloud.com>
2020-08-22 19:03:44 +0900

    懺悔相談所のベース実装

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,35 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react'
+import logo from './logo.svg'
+import './App.css'
+import axios from 'axios'
+
+interface Data {
+  answer: string
+  forced: boolean
+  image: string
+}
 
 function App() {
+  const [data, setData] = useState<Data>()
+  const fetchData = async () => {
+    const result = (await axios('https://yesno.wtf/api')) as any
+    setData(result.data)
+  }
+
+  const handleOnClick = () => {
+    fetchData()
+  }
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <p>お悩み相談所</p>
+        <div>{data?.answer}</div>
+        <img src={data?.image} />
+        <button onClick={handleOnClick}>懺悔する！</button>
       </header>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
